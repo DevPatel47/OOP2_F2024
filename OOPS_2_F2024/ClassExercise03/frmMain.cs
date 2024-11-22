681349c177298dd77aa38ea0a783a816bbddaa2a
DevPatel47 <dev080405.patel@gmail.com>
2024-11-22 09:08:15 -0500

    Class Exercise - 3: Comments added

--- OOPS_2_F2024/ClassExercise03/frmMain.cs
+++ OOPS_2_F2024/ClassExercise03/frmMain.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/* **************************************
+* COSC2100 Section 4 F2024
+* Dev Mayurkumar Patel
+* Nov 22, 2024
+* Main Form Class File
+* ***********************************  */
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,16 +20,22 @@ namespace ClassExercise03
 {
     public partial class frmMain : Form
     {
+        /// <summary>
+        /// To initialize form
+        /// </summary>
         public frmMain()
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Form load event method
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void frmMain_Load(object sender, EventArgs e)
         {
             // TODO: This line of code loads data into the 'sportleaguesDataSet.teams' table. You can move, or remove it, as needed.
             this.teamsTableAdapter.Fill(this.sportleaguesDataSet.teams);
-
             if (cbo_teams.SelectedItem != null)
             {
                 int teamValue = (int)cbo_teams.SelectedValue;
@@ -29,7 +43,11 @@ namespace ClassExercise03
 
             }
         }
-
+        /// <summary>
+        /// Combo box index change event method
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void cbo_teams_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (cbo_teams.SelectedItem != null) 
